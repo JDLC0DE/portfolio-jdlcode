76d80da8436dd60385a7389c650d364adc56db27
Joseph Francisco De La Cruz Rivas <jfdelacruz@avatar-global.com>
2025-01-27 15:19:08 -0500

    feat: add experiencie

--- integration/data/constants/experiencies.ts
+++ integration/data/constants/experiencies.ts
@@ -5,22 +5,22 @@ export const EXPERIENCIES = [
     period: 'Ago 2023 - Presente',
     image: '/images/experience/avatar_global_logo.jpg',
     description:
-      'Desarrollo de aplicaciones web y móviles, mantenimiento de aplicaciones existentes y soporte técnico a usuarios.'
+      'Lideré un equipo en el desarrollo frontend utilizando React.js, Material UI y TypeScript, enfocado en la lógica y maquetación de interfaces. Implementé la gestión de estados con useContext y Redux Toolkit, y realicé pruebas de rendimiento para optimizar la aplicación. Supervisé los despliegues a producción y la integración de datos mediante APIs REST, además de adquirir experiencia en HCL Commerce y Salesforce.'
   },
   {
     title: 'Ingeniero de Software',
     company: 'RAVN',
-    period: 'Ene 2024 - Jun 2024',
+    period: 'Feb 2024 - Jun 2024',
     image: '/images/experience/ravnhq_logo.jpg',
     description:
-      'Desarrollo de aplicaciones web y móviles, mantenimiento de aplicaciones existentes y soporte técnico a usuarios.'
+      'Desarrollé interfaces móviles utilizando Flutter y React Native, y vistas web con React y Vite, priorizando la optimización del rendimiento mediante la reducción de renders innecesarios y disminución del tamaño de las aplicaciones. Actué como consultor en la construcción de pruebas Backend, asegurando la calidad del sistema. Además, gestioné la integración de datos mediante APIs REST y GraphQL, garantizando una comunicación eficiente entre los servicios y las aplicaciones.'
   },
   {
     title: 'Desarrollador Frontend',
     company: 'Digenio LAB',
-    period: 'Ago 2023 - Ago 2025',
+    period: 'Feb 2022 - Ago 2023',
     image: '/images/experience/digeniolab_logo.jpg',
     description:
-      'Desarrollo de aplicaciones web y móviles, mantenimiento de aplicaciones existentes y soporte técnico a usuarios.'
+      'Como líder de equipo, diseñé y maqueté aplicaciones cliente y administrativas utilizando Next.js, TailwindCSS y TypeScript, implementando manejo de estados con useContext y temas personalizados como modo oscuro y diseño neumórfico. Supervisé pruebas end-to-end, rendimiento de la aplicación y desarrollo de componentes personalizados, garantizando alta calidad en cada iteración. Además, gestioné la conexión de datos mediante GraphQL y Apollo Client, asegurando una integración eficiente y escalable.'
   }
 ]
